7579bc0206aab6ceb7d41a1392aa63fcffa52239
Nazariy <nazariypanochko@gmail.com>
2025-08-10 13:52:31 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,18 +7,24 @@ wall.addEventListener('click', (e) => {
   const wallRect = wall.getBoundingClientRect();
 
   const spiderWidth = spider.offsetWidth;
-  const spiderHeigth = spider.offsetHeight;
+  const spiderHeight = spider.offsetHeight;
 
   const clickX = e.clientX - wallRect.left;
   const clickY = e.clientY - wallRect.top;
 
   let spiderX = clickX - spiderWidth / 2;
-  const spiderY = clickY - spiderHeigth / 2;
+  let spiderY = clickY - spiderHeight / 2;
 
   if (spiderX < 0) {
     spiderX = 0;
   } else if (spiderX + spiderWidth > wall.offsetWidth) {
     spiderX = wall.offsetWidth - spiderWidth;
+  }
+
+  if (spiderY < 0) {
+    spiderY = 0;
+  } else if (spiderY + spiderHeight > wall.offsetHeight) {
+    spiderY = wall.offsetHeight - spiderHeight;
   }
 
   spider.style.left = `${spiderX}px`;
